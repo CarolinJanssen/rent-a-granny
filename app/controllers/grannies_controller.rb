59d4f6d362f1667349c0e7c1c67c5d44fe8c2f57
David Wise <davidmarkwise@gmail.com>
2020-02-25 14:42:56 +0100

    Corrections to grannies controller

--- app/controllers/grannies_controller.rb
+++ app/controllers/grannies_controller.rb
@@ -4,10 +4,12 @@ class GranniesController < ApplicationController
   end
 
   def show
+    authorize @granny
   end
 
   def new
     @granny = Granny.new
+    authorize @granny
   end
 
   def create
@@ -25,16 +27,19 @@ class GranniesController < ApplicationController
   end
 
   def update
+    authorize @granny
     if @granny.update(granny_params)
       # redirect_to
     else
       render :edit
     end
+
   end
 
   def destroy
     @granny.destroy
     # redirect_to
+    authorize @granny
   end
 
   private

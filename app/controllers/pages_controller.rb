9f86fc53f446c14da196c28c930d99a7278dbfd3
Christoph Willisch <christoph.j.willisch@gmail.com>
2020-02-26 16:24:47 +0100

    created dashboard view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
+require 'date'
+
 class PagesController < ApplicationController
     skip_before_action :authenticate_user!, only: :home
   def home
   end
 
   def dashboard
+    @grannies = Granny.all
+    @future_appointments = Appointment.where("end_date > ?", Date.today)
+    @past_appointments = Appointment.where("end_date < ?", Date.today)
   end
 end

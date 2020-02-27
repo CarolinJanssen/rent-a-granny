b89a923a3945136ade58f3eee5ef57f0a04ae48f
Carolin Janssen <c.j@hempberg.com>
2020-02-27 13:03:33 +0100

    added connections

--- app/models/granny.rb
+++ app/models/granny.rb
@@ -3,4 +3,13 @@ class Granny < ApplicationRecord
   belongs_to :activity
   belongs_to :user
   # has_one_attached :photo
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :district, presence: true
+  validates :description, presence: true
+  validates :important_notes, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :age, presence: true, numericality: { only_integer: true }
 end
+# :photo

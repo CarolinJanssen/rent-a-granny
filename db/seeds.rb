fdee6aebcba7588052a0597f1c5d0fb0433d3b27
Carolin Janssen <c.j@hempberg.com>
2020-02-28 13:52:56 +0100

    changed the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning database"
+Appointment.destroy_all
 Granny.destroy_all
 Activity.destroy_all if Rails.env.development?
 

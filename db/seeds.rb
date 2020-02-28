4893676e516653690c7b6785ec35923869235302
David Wise <davidmarkwise@gmail.com>
2020-02-28 10:57:47 +0100

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,22 @@ Activity.create(name: "Playground")
 # p 'seeds done right'
 Granny.destroy_all
 
-granny_1 = Granny.new(first_name: "Eunice", last_name: "Blake", district: "Mitte", description: "Drawing", important_notes: "Bad back", price: 20, age: 80)
+granny_1 = Granny.new(first_name: "Eunice", last_name: "Blakely", district: "Mitte", description: "Drawing", important_notes: "Bad back", price: 20, age: 80)
+
+granny_2 = Granny.new(first_name: "Meryl", last_name: "Streep", district: "Mitte", description: "Reading", important_notes: "Please don't ask me to lift more than 5 kg, please.", price: 20, age: 72)
+
+granny_3 = Granny.new(first_name: "Rosemary", last_name: "Darcey-Grynnell DBE", district: "Mitte", description: "Doing Homework", important_notes: "No funny stuff!", price: 25, age: 91)
+
+granny_4 = Granny.new(first_name: "Elisabeth", last_name: "Higgins", district: "Friedrichshain-Kreuzberg", description: "Crafting", important_notes: "Apartment is very small, limit 2 children.", price: 10, age: 76)
+
+granny_5 = Granny.new(first_name: "Jane", last_name: "Fonda", district: "Schöneberg", description: "Swimming", important_notes: "No boys, please", price: 19, age: 89)
+
+granny_6 = Granny.new(first_name: "Eunice", last_name: "Blake", district: "Mitte", description: "Reading", important_notes: "My legs are tricky!", price: 25, age: 88)
+
 granny_1.activity = drawing
-granny_1.user = user_1
-granny_1.save!
+
+# granny_1.user = user_1
+# granny_1.save!
+
 
 # user_1 = User.create(first_name: Huey)
